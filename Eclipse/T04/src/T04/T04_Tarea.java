77d169ee9196df2fba3112b90df119e9f2f670b3
Santis079 <s.riveracamacho079@gmail.com>
2024-02-05 11:39:00 +0100

    commit antes del patio

---
+++ Eclipse/T04/src/T04/T04_Tarea.java
@@ -0,0 +1,23 @@
+package T04;
+
+public class T04_Tarea {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=10;
+		int suma=a+b;
+		int resta=a-b;
+		int multiplicacion=a*b;
+		double division=a/b;
+		double modulo=a%b;
+		
+System.out.println("la suma es "+suma);
+System.out.println("la resta es " +resta);
+System.out.println("la multiplicacion es "+multiplicacion);
+System.out.println("la division es " +division);
+System.out.println("el modulo es " +modulo);
+		
+
+	}
+
+}
---
+++ Eclipse/T04/src/T04/T04_Tarea_2.java
@@ -0,0 +1,10 @@
+package T04;
+
+public class T04_Tarea_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
