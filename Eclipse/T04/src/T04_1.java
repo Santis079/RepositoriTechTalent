540bda4120799e762e2611bb5cda781f6555c1a9
Santis079 <s.riveracamacho079@gmail.com>
2024-02-05 10:17:09 +0100

    comit T04_2

--- Eclipse/T04/src/T04_1.java
+++ Eclipse/T04/src/T04_1.java
@@ -1,4 +1,19 @@
 
 public class T04_1 {
 
+	public static void main(String[] args) {
+		
+		int operador=10;
+		int x=0;			
+		operador++	;	
+		operador++;
+		operador--;
+		x=operador++;
+		x=++operador;
+				
+				System.out.println(operador);
+				System.out.println(operador++);
+				System.out.println(++operador);
+	}
+
 }
