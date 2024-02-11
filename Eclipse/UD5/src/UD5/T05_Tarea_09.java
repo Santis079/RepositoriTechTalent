819bf4d20b60f75c2d1e5e0221a1c4be1b61f1ef
Santis079 <s.riveracamacho079@gmail.com>
2024-02-11 22:53:09 +0100

    commit siguiente relevo
    Relevo en caja poker y el lio k ha hecho garzon

---
+++ Eclipse/UD5/src/UD5/T05_Tarea_09.java
@@ -0,0 +1,18 @@
+package UD5;
+public class T05_Tarea_09 {
+
+    public static void main(String[] args) {
+        
+        int numerito = 1;
+
+        while (numerito <= 100) {
+            if (numerito % 2 == 0 && numerito % 3 ==0) {
+                System.out.println(numerito);
+            }
+            
+            numerito++;
+        }
+    }
+}
+
+   
