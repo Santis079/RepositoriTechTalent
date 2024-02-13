22319c4a547e332e60fd2b242365e1f297144d89
Santis079 <s.riveracamacho079@gmail.com>
2024-02-13 14:02:09 +0100

    commit endclass_3

--- Eclipse/UD5/src/UD5/T05_Extra_3.java
+++ Eclipse/UD5/src/UD5/T05_Extra_3.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class T05_Extra_3 {
 
     public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog("Ingrese un número:");
+        String input = JOptionPane.showInputDialog("Ingrese un numero:");
         int numero = Integer.parseInt(input);
 
         if (numero >= 0) {
@@ -14,9 +14,9 @@ public class T05_Extra_3 {
                 suma += i;
             }
 
-            JOptionPane.showMessageDialog(null, "La suma de los números anteriores al " + numero + " es: " + suma);
+            JOptionPane.showMessageDialog(null, "La suma del numero y los antoriores es " + numero + " es: " + suma);
         } else {
-            JOptionPane.showMessageDialog(null, "Ingrese un número positivo.");
+            JOptionPane.showMessageDialog(null, "pon un numero positivo.");
         }
     }
 }
